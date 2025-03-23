13b5c25d89ea901b96b5dcd2f24c9d5178415c4b
het07-patel <76025681+Het-07@users.noreply.github.com>
2025-03-22 23:01:00 -0300

    Add(C1): dummy commit in app.py

--- C1_app.py
+++ C1_app.py
@@ -8,7 +8,6 @@ PERSISTENT_STORAGE_PATH = "/het_PV_dir"
 
 CONTAINER2_URL = "http://container2-service:8000/compute"
 
-# store-file api
 @app.post("/store-file")
 async def store_file(request: Request):
     try:
@@ -30,7 +29,6 @@ async def store_file(request: Request):
     except:
         return JSONResponse(status_code=400, content={"file": None, "error": "Invalid JSON input."})
 
-# calculate api
 @app.post("/calculate")
 async def calculate(request: Request):
     try:
